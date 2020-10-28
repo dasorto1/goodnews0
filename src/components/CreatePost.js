77c7754f727171af698ca19a0a2f37aff408436c
dasorto <59189422+dasorto@users.noreply.github.com>
2020-10-27 23:16:02 -0400

    fifths

---
+++ src/components/CreatePost.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreatPost = (props) => {
+    return (
+        <h1>Create Post</h1>
+    )
+}
+
+export default CreatePost;
--- src/containers/main/addPost/addPostScreen.js
+++ src/containers/main/addPost/addPostScreen.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet, Alert} from 'react-native';
 import palette from 'res/palette';
 
-export default function addPostScreen() {
+export default function addPostScreen() { 
   return (
-    <View style={palette.container.center}>
-      <Text style={palette.text}>Add Post</Text>
+    <View style={{ flex: 1, marginTop: 60 }}>
+    <View>
+        <Button title = "Add an image of your news article"
+          onPress={() => Alert.alert('Button pressed')}
+          style={{
+            alignItems: 'center',
+            padding: 10,
+            margin: 30
+          }}/>
+          
+      
     </View>
+<<<<<<< HEAD
   );
 }
+=======
+    <View style={{ marginTop: 80, alignItems: 'center' }}>
+      <Text style={styles.titleText}> News Details</Text>
+      <TextInput
+        placeholder='Enter title of the article'
+        style={{ margin: 20 }}
+        
+      
+      />
+      <TextInput
+        placeholder='Enter description'
+        style={{ margin: 20 }}
+      
+      />
+      <Button
+        title="Add news"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      />
+      
+    </View>
+
+  </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+>>>>>>> 071a66a... fifth
